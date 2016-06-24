0c6b236bebf552c62ac43dbce0c7df120eab2552
dhruvil <dd.droid@yahoo.com>
2016-06-24 20:31:53 +0530

    h

--- app/src/main/java/info/androidsnippet/firebaselr/LoginActivity.java
+++ app/src/main/java/info/androidsnippet/firebaselr/LoginActivity.java
@@ -2,13 +2,29 @@ package info.androidsnippet.firebaselr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private EditText edEmail;
+    private EditText edPassword;
+    private TextView txtSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Lets find some components
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        edEmail = (EditText) findViewById(R.id.edEmail);
+        edPassword = (EditText) findViewById(R.id.edPassword);
+        txtSignup = (TextView) findViewById(R.id.txtSignup);
+
 
     }
 }
